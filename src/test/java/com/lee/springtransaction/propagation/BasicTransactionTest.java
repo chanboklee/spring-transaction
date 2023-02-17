d58ab78317af7488b8df4dcb344343274d8ba460
chanboklee <chanboklee@naver.com>
2023-02-17 17:25:46 +0900

    Fix: spring-transaction - spring transaction propagation -> internal rollback

--- src/test/java/com/lee/springtransaction/propagation/BasicTransactionTest.java
+++ src/test/java/com/lee/springtransaction/propagation/BasicTransactionTest.java
@@ -1,6 +1,7 @@
 package com.lee.springtransaction.propagation;
 
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.UnexpectedRollbackException;
 import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
 
 import javax.sql.DataSource;
@@ -105,6 +107,44 @@ public class BasicTransactionTest {
         log.info("내부 트랜잭션 커밋");
         transactionManager.commit(inner);
 
-        log.info("외부 트랜잭션 롤백");        transactionManager.rollback(outer);
+        log.info("외부 트랜잭션 롤백");
+        transactionManager.rollback(outer);
+    }
+
+    /**
+     * [내부 트랜잭션]
+     * 트랜잭션 매니저는 커밋 시점에 신규 트랜잭션 여부에 따라 다르게 동
+     * 내부 트랜잭션은 신규 트랜잭션이 아니기 때문에 실제 롤백을 호출하지 않는다.
+     * 실제 커넥션에 커밋이나 롤백을 호출하면 물리 트랜잭션이 끝나버린다.
+     * 내부 트랜잭션은 물리 트랜잭션을 롤백하지 않는 대신에 트랜잭션 동기화 매니저에 'rollbackOnly=true' 라는 표시를 한다.
+     *
+     * [외부 트랜잭션]
+     * 외부 트랜잭션은 신규 트랜잭션이다. 따라서 DB 커넥션에 실제 커밋을 호출해야 한다.
+     * 이때 먼저 트랜잭션 동기화 매니저에 롤백 전용(rollbackOnly=true) 표시가 있는지 확인한다.
+     * 롤백 전용 표시가 있으면 물리 트랜잭션을 커밋하는 것이 아니라 롤백한다.
+     *
+     * [정리]
+     * 모든 논리 트랜잭션이 커밋되어야 물리 트랜잭션이 커밋된다.
+     * 하나의 논리 트랜잭션이라도 롤백되면 물리 트랜잭션은 롤백된다.
+     *
+     * 논리 트랜잭션이 하나라도 롤백되면 물리 트랜잭션은 롤백된다.
+     * 내부 논리 트랜잭션이 롤백되면 롤백 전용 마크를 표시한다.
+     * 외부 트랜잭션을 커밋할 때 롤백 전용 마크를 확인한다. 롤백 전용 마크가 표시되어 있으면 물리 트랜잭션을 롤백하고
+     * UnexpectedRollbackException 예외를 던진다.
+     */
+    @Test
+    void inner_rollback(){
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = transactionManager.getTransaction(new DefaultTransactionAttribute());
+
+        log.info("내부 트랜잭션 시작");
+        TransactionStatus inner = transactionManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("내부 트랜잭션 롤백");
+        transactionManager.rollback(inner); // rollback-only marked
+
+        log.info("외부 트랜잭션 커밋");
+        transactionManager.commit(outer);
+        Assertions.assertThatThrownBy(() -> transactionManager.commit(outer))
+                .isInstanceOf(UnexpectedRollbackException.class);
     }
 }
