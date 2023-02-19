1a374b6ea77106da3e2308abfa3edf91449a9a45
chanboklee <chanboklee@naver.com>
2023-02-19 14:05:08 +0900

    Fix: spring-transaction - utilize spring transaction propagation -> recovery REQUIRED

--- src/test/java/com/lee/springtransaction/propagation/MemberServiceTest.java
+++ src/test/java/com/lee/springtransaction/propagation/MemberServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.UnexpectedRollbackException;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
@@ -108,6 +109,32 @@ class MemberServiceTest {
         // when
         assertThatThrownBy(() -> memberService.joinV1(username))
                 .isInstanceOf(RuntimeException.class);
+
+        // then -> 모든 데이터가 롤백된다.
+        assertTrue(memberRepository.find(username).isEmpty());
+        assertTrue(logRepository.find(username).isEmpty());
+    }
+
+    /**
+     * memberService        -> @Transactional : ON
+     * memberRepository     -> @Transactional : ON
+     * logRepository        -> @Transactional : ON Exception
+     *
+     * [정리]
+     * 논리 트랜잭션 중 하나라도 롤백되면 전체 트랜잭션은 롤백된다.
+     * 내부 트랜잭션이 롤백되었는데, 외부 트랜잭션이 커밋되면 UnexpectedRollbackException 예외 발생
+     * rollbackOnly 상황에서 커밋이 발생하면 UnexpectedRollbackException 예외 발생
+     *
+     * 정상 흐름으로 반환하였어도 LogRepository에서 이미 rollbackOnly marked하였으므로 롤백된다.
+     */
+    @Test
+    void recoverException_fail(){
+        // given
+        String username = "로그예외_recoverException_fail";
+
+        // when
+        assertThatThrownBy(() -> memberService.joinV2(username))
+                .isInstanceOf(UnexpectedRollbackException.class);
 
         // then -> 모든 데이터가 롤백된다.
         assertTrue(memberRepository.find(username).isEmpty());
